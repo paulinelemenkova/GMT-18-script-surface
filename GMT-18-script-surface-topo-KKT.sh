0ecc5e39f071e59f8c9f687c12bf7e2f4ab419a0
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-05-22 16:16:23 +0800

    Add files via upload
    added variant with shading illumination azimuth=45 degrees

--- GMT-18-script-surface-topo-KKT.sh
+++ GMT-18-script-surface-topo-KKT.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Purpose: Surface modelling of the topography along the Kuril-Kamchatka Trench
-# GMT modules: gmtset, gmtdefaults, makecpt, gmtinfo, blockmean, surface, grdimage, psbasemap, psscale, gmtlogo, pstext, psconvert
+# GMT modules: gmtset, gmtdefaults, makecpt, gmtinfo, blockmean, surface, grdgradient, grdimage, psbasemap, psscale, gmtlogo, pstext, psconvert
 # Step-1. Generate a file
-ps=SurfaceKKT.ps
+ps=SurfaceTKKT.ps
 # Step-2. GMT set up
 gmt set FORMAT_GEO_MAP=dddF \
     MAP_FRAME_PEN=dimgray \
@@ -29,34 +29,37 @@ gmt makecpt -CGMT_sealand.cpt -V -T-10000/2200 > surface.cpt
 gmt blockmean topo_KKT.xyz -R144/162/40/51 -I1m -Vv > topo_KKT_BM.xyg
 # Step-8. Generate grid from xyz table format
 gmt surface topo_KKT_BM.xyg -R144/162/40/51 -T0.25 -I30s -GSurface_KKT.nc -Vv
-# Step-9. Make raster image
-gmt grdimage Surface_KKT.nc -Csurface.cpt -R144/162/40/51 -JM6i -P -I+a15+ne0.75 -Xc -K > $ps
-# Step-10. Add grid
+# Step-9. Make gradient illumination with azimuth 45 degree
+gmt grdgradient Surface_KKT.nc -GSurface_KKT.int -A0/45 -Ne1 -fg
+# Step-10. Make raster image
+#gmt grdimage Surface_KKT.nc -Csurface.cpt -R144/162/40/51 -JM6i -P -I+a15+ne0.75 -Xc -K > $ps
+gmt grdimage Surface_KKT.nc -Csurface.cpt -R144/162/40/51 -JM6i -P -ISurface_KKT.int -Xc -K > $ps
+# Step-11. Add grid
 gmt psbasemap -R -J \
     -Bpxg6f2a2 -Bpyg6f1a2 -Bsxg2 -Bsyg2 \
     -B+t"Surface modelling of the topography along the Kuril-Kamchatka Trench" -O -K >> $ps
-# Step-11. Add scale, directional rose
+# Step-12. Add scale, directional rose
 gmt psbasemap -R -J \
     --FONT=8p,Palatino-Roman,dimgray \
     --MAP_TITLE_OFFSET=0.3c \
     -Tdx0.8c/11.8c+w0.3i+f2+l+o0.15i \
     -Lx5.3i/-0.5i+c50+w400k+l"Mercator projection. Scale (km)"+f \
     -UBL/-15p/-40p -O -K >> $ps
-# Step-12. Add color legend
+# Step-13. Add color legend
 gmt psscale -R -J -Csurface.cpt \
     -Dg144/40+w12.7c/0.4c+v+o-1.8/0.2c+ml  \
     --FONT_LABEL=8p,Helvetica,dimgray \
     --FONT_ANNOT_PRIMARY=5p,Helvetica,dimgray \
     -Baf+l"Surface topographic model color scale" \
     -I0.2 -By+lm -O -K >> $ps
-# Step-13. Add GMT logo
+# Step-14. Add GMT logo
 gmt logo -Dx6.2/-2.2+o0.1i/0.1i+w2c -O -K >> $ps
-# Step-14. Add subtitle
+# Step-15. Add subtitle
 gmt pstext -R0/10/0/15 -JX10/10 -X0.0c -Y5.5c -N -O \
     -F+f10p,Palatino-Roman,black+jLB >> $ps << EOF
 0.7 13.7 Modelling: GMT surface module, tension factor of the continuous curvature splines 0.25
 0.0 13.2 Input raw table data: global 1-min grid resolution in ASCII XYZ-format, applied blockmean filter.
-2.7 12.7 Output spatial model: 30-sec grid spacing in netCDF format
+0.5 12.7 Output spatial model: 30-sec grid spacing in netCDF format, shading azimuth gradient: 45\232
 EOF
-# Step-15. Convert to image file using GhostScript
-gmt psconvert SurfaceKKT.ps -A0.2c -E720 -P -Tj -Z
+# Step-16. Convert to image file using GhostScript
+gmt psconvert SurfaceTKKT.ps -A0.2c -E720 -P -Tj -Z
